5055288e6402cc19b3ed42eb3b1d5c70a4a73e2a
Guan Li <guanLi@student.unsw.edu.au>
2025-06-22 19:06:51 +1000

    init project

---
+++ my-app/app/CarouselDemo.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import Carousel from "@/components/ui/carousel";
+export default function GameCarousel() {
+  const router = useRouter();
+
+  const slideData = [
+     {
+      title: "Mystic Mountains",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/mountain"
+    },
+    {
+      title: "Urban Dreams",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/mountain"
+    },
+    {
+      title: "Neon Nights",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/mountain"
+    },
+    {
+      title: "Desert Whispers",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      path: "/mountain"
+    },
+  ];
+
+  const buttonClick = (slide) => {
+    router.push(slide.path);
+  };
+
+  return (
+    <div className="relative overflow-hidden w-full h-full py-20">
+      <Carousel 
+        slides={slideData} 
+        onButtonClick={(slide) => buttonClick(slide)}
+      />
+    </div>
+  );
+}
---
+++ my-app/app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import CarouselDemo from "./CarouselDemo"
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <CarouselDemo/>
+    </div>
+  )
+}
