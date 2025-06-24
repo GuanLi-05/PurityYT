2f75800c0d5e8e4e23082821889ad2c1b6720480
Guan Li <guanLi@student.unsw.edu.au>
2025-06-24 22:30:32 +1000

    bcrypt and prisma install

---
+++ my-app/backend/registration.js
@@ -0,0 +1,47 @@
+import bcrypt from 'bcrypt'
+import express from 'express'
+
+///////////////////////////////////
+// Route exporting for app.js
+///////////////////////////////////
+
+export const handleRegisterRouter = express.Router();
+
+///////////////////////////////////
+// Routes
+///////////////////////////////////
+
+
+
+const saltRounds = 10;
+const hashedPassword = await bcrypt.hash(password, saltRounds);
+const isValid = await bcrypt.compare(submittedPassword, user.password)
+
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // Create a user
+  const newUser = await prisma.user.create({
+    data: {
+      email: "alice@example.com",
+      name: "Alice",
+      password: "hashedpassword123",
+    },
+  })
+
+  console.log("Created user:", newUser)
+
+  // Query user by email
+  const user = await prisma.user.findUnique({
+    where: { email: "alice@example.com" },
+  })
+
+  console.log("Found user:", user)
+}
+
+main()
+  .catch(e => console.error(e))
+  .finally(async () => await prisma.$disconnect())
